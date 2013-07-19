98069a28b4122558067061f5e38e70297dd613db
Robert McDowell <bert@electrictophat.com>
2013-07-19 17:40:56 +0100

    Added support to update a project. Moved the external frameworks over to reside in the Frameworks folder.

--- tools/project-creator/create_project_2.py
+++ tools/project-creator/create_project_2.py
@@ -14,15 +14,22 @@ context = {
 "src_project_path"  : "undefined",
 "dst_project_path"  : "undefined",
 "script_dir"        : "undefined",
+"dst_upgrade_path"  : "undefined",
 }
 platforms_list = []
 
+frameworksDest = "proj/Frameworks"
 frameworks = {
 "cocos2dx" : "../../cocos2dx",
 "CocosDenshion" : "../../CocosDenshion",
-"external" : "../../external",
+"Box2D" : "../../external/Box2D",
+"chipmunk" : "../../external/chipmunk",
+"emscrpten" : "../../external/emscripten",
+"libwebsockets" : "../../external/libwebsockets",
+"sqlite3" : "../../external/sqlite3",
 "extentions" : "../../extensions",
 }
+
 
 content = {
 "Classes" : "Classes",
@@ -43,10 +50,13 @@ def dumpUsage():
     print "  -package   PACKAGE_NAME          Package name, for example: com.MyCompany.MyAwesomeGame"
     print "  -language  PROGRAMING_LANGUAGE   Major programing lanauge you want to used, should be [cpp | lua | javascript]"
     print "  -path      OUTPUT_PATH           Optional destination path"
+    print "  -upgrade   PROJECT_PATH          Path to the project to upgrade"
     print "  -help      HELP                  Display this message"
     print ""
     print "Sample 1: ./create_project.py -project MyGame -package com.MyCompany.AwesomeGame"
     print "Sample 2: ./create_project.py -project MyGame -package com.MyCompany.AwesomeGame -language javascript"
+    print "Sample 3: ./create_project.py -project MyGame -package com.MyCompany.AwesomeGame -path path/to/project"
+    print "Sample 4: ./create_project.py -project MyGame -upgrade path/to/project"
     print ""
 
 def checkParams(context):
@@ -55,7 +65,11 @@ def checkParams(context):
     global platforms_list
     
     # invalid invoke, tell users how to input params
-    if len(sys.argv) < 7:
+    if len(sys.argv) <= 0:
+        dumpUsage()
+        sys.exit()
+
+    if "-help" == sys.argv[0]:
         dumpUsage()
         sys.exit()
     
@@ -64,6 +78,9 @@ def checkParams(context):
 
     # find our params
     for i in range(1, len(sys.argv)):
+        if "-upgrade" == sys.argv[i]:
+            # read the next param as upgrade_path
+            context["dst_upgrade_path"] = sys.argv[i+1]
         if "-project" == sys.argv[i]:
             # read the next param as project_name
             context["dst_project_name"] = sys.argv[i+1]
@@ -77,52 +94,60 @@ def checkParams(context):
             # change the destination path
             context["dst_project_path"] = sys.argv[i+1]
 
-    # append the project name to the path
-    context["dst_project_path"] = os.path.join(context["dst_project_path"], context["dst_project_name"])
-    
     # pinrt error log our required paramters are not ready
-    raise_error = False
-    if context["dst_project_name"] == "undefined":
-        print "Invalid -project parameter"
-        raise_error = True
-    if context["dst_package_name"] == "undefined":
-        print "Invalid -package parameter"
-        raise_error = True
-    if context["language"] == "undefined":
-        print "Invalid -language parameter"
-        raise_error = True
-    if raise_error != False:
-        sys.exit()
-                                 
-    # fill in src_project_name and src_package_name according to "language"
-    if ("cpp" == context["language"]):
-        context["src_project_name"] = "HelloCpp"
-        context["src_package_name"] = "org.cocos2dx.hellocpp"
-        context["src_project_path"] = os.getcwd() + "/../../template/multi-platform-cpp"
-        platforms_list = ["ios",
-                          "android",
-                          "win32",
-                          "mac",
-                          "blackberry",
-                          "linux",
-                          "marmalade"]
-    elif ("lua" == context["language"]):
-        context["src_project_name"] = "HelloLua"
-        context["src_package_name"] = "org.cocos2dx.hellolua"
-        context["src_project_path"] = os.getcwd() + "/../../template/multi-platform-lua"
-        platforms_list = ["ios",
-                          "android",
-                          "win32",
-                          "blackberry",
-                          "linux",
-                          "marmalade"]
-    elif ("javascript" == context["language"]):
-        context["src_project_name"] = "HelloJavascript"
-        context["src_package_name"] = "org.cocos2dx.hellojavascript"
-        context["src_project_path"] = os.getcwd() + "/../../template/multi-platform-js"
-        platforms_list = ["ios",
-                          "android",
-                          "win32"]
+    if context["dst_upgrade_path"] == "undefined":
+        # append the project name to the path
+        context["dst_project_path"] = os.path.join(context["dst_project_path"], context["dst_project_name"])
+
+        raise_error = False
+        if context["dst_project_name"] == "undefined":
+            print "Invalid -project parameter"
+            raise_error = True
+        if context["dst_package_name"] == "undefined":
+            print "Invalid -package parameter"
+            raise_error = True
+        if context["language"] == "undefined":
+            print "Invalid -language parameter"
+            raise_error = True
+        if raise_error != False:
+            dumpUsage()
+            sys.exit()
+                                     
+        # fill in src_project_name and src_package_name according to "language"
+        if ("cpp" == context["language"]):
+            context["src_project_name"] = "HelloCpp"
+            context["src_package_name"] = "org.cocos2dx.hellocpp"
+            context["src_project_path"] = os.getcwd() + "/../../template/multi-platform-cpp"
+            platforms_list = ["ios",
+                              "android",
+                              "win32",
+                              "mac",
+                              "blackberry",
+                              "linux",
+                              "marmalade"]
+        elif ("lua" == context["language"]):
+            context["src_project_name"] = "HelloLua"
+            context["src_package_name"] = "org.cocos2dx.hellolua"
+            context["src_project_path"] = os.getcwd() + "/../../template/multi-platform-lua"
+            platforms_list = ["ios",
+                              "android",
+                              "win32",
+                              "blackberry",
+                              "linux",
+                              "marmalade"]
+        elif ("javascript" == context["language"]):
+            context["src_project_name"] = "HelloJavascript"
+            context["src_package_name"] = "org.cocos2dx.hellojavascript"
+            context["src_project_path"] = os.getcwd() + "/../../template/multi-platform-js"
+            platforms_list = ["ios",
+                              "android",
+                              "win32"]
+
+    else:
+        # append the project name to the path
+        context["src_project_path"] = os.getcwd()
+        context["dst_upgrade_path"] = os.path.join(context["dst_upgrade_path"], context["dst_project_name"])
+
 # end of checkParams(context) function
 
 def ireplace(old, new, text):
@@ -233,71 +258,101 @@ def processPlatformProjects(platform):
 checkParams(context)
 # import pprint
 # pprint.pprint(context)
+print context["dst_upgrade_path"]
+if context["dst_upgrade_path"] != "undefined":
 
-# copy "lauguage"(cpp/lua/javascript) platform.proj into cocos2d-x/projects/<project_name>/folder
-if (os.path.exists(context["dst_project_path"]) == True):
-    print "Error:" + context["dst_project_path"] + " folder already exists"
-    print "Please remove the old project or choose a new PROJECT_NAME in -project parameter"
-    sys.exit()
+    if (os.path.exists(context["dst_upgrade_path"]) == True):
+
+        # define the frameworks path
+        frameworksPath = os.path.join ( context["dst_upgrade_path"], frameworksDest )
+        print frameworksPath
+        if (os.path.exists(frameworksPath) == True):
+        
+            # copy frameworks into the folder
+            for framework in frameworks.keys():
+                frameworkPath = frameworks[framework]
+                sourcePath = context["src_project_path"]+"/"+frameworkPath
+                destinationPath = os.path.join ( frameworksPath, framework )
+                
+                if (os.path.exists(destinationPath) == True):
+                    shutil.rmtree(destinationPath, ignore_errors=True);
+
+                shutil.copytree( sourcePath, destinationPath, symlinks=False, ignore=ignore_patterns('.*'))
+                print "Updated framework: " + framework
+
+
+            print "Upgrade project complete at path: " + context["dst_upgrade_path"]
+
+        else:
+            print "Error:" + frameworksPath + " folder does not exists"
+    else:
+        print "Error:" + context["dst_upgrade_path"] + " folder does not exists"
+
 else:
-    shutil.copytree(context["src_project_path"], context["dst_project_path"], symlinks=False, ignore=ignore_patterns('.*'))
 
-# call process_proj from each platform's script folder          
-for platform in platforms_list:
-    processPlatformProjects(platform)
-#    exec "import %s.handle_project_files" % (platform)
-#    exec "%s.handle_project_files.handle_project_files(context)" % (platform)
+    # copy "lauguage"(cpp/lua/javascript) platform.proj into cocos2d-x/projects/<project_name>/folder
+    if (os.path.exists(context["dst_project_path"]) == True):
+        print "Error:" + context["dst_project_path"] + " folder already exists"
+        print "Please remove the old project or choose a new PROJECT_NAME in -project parameter"
+        sys.exit()
+    else:
+        shutil.copytree(context["src_project_path"], context["dst_project_path"], symlinks=False, ignore=ignore_patterns('.*'))
 
-# check to see if the path is not the default path
-if not "/../../projects/" in context["dst_project_path"]:
-    # define the frameworks path
-    frameworksDest = "proj/Frameworks"
-    frameworksPath = os.path.join ( context["dst_project_path"], frameworksDest )
-    # create a frameworks path
-    os.makedirs ( frameworksPath )
-
-    # relative path to the project
-    fixPathOrig = []
-    fixPathDest = []
-
-    # copy frameworks into the folder
-    for framework in frameworks.keys():
-        frameworkPath = frameworks[framework]
-        sourcePath = context["src_project_path"]+"/"+frameworkPath
-        destinationPath = os.path.join ( frameworksPath, framework )
-        shutil.copytree( sourcePath, destinationPath, symlinks=False, ignore=ignore_patterns('.*'))
-        fixPathOrig.append ( frameworkPath )
-        fixPathOrig.append ( frameworkPath.replace("/", "\\") )
-        fixPathDest.append ( os.path.join ( frameworksDest, framework ) )
-        fixPathDest.append ( os.path.join ( frameworksDest, framework ).replace("/", "\\") )
-        print "Copied framework: " + framework
-
-    #move the content folders
-    contentDest = "proj"
-    contentsPath = os.path.join ( context["dst_project_path"], contentDest )
-    for contentKey in content:
-        contentValue = content[contentKey]
-        sourcePath = context["dst_project_path"]+"/"+contentValue
-        destinationPath = os.path.join ( contentsPath, contentKey )
-        shutil.move( sourcePath, destinationPath )
-        fixPathOrig.append ( "../"+contentValue )
-        fixPathOrig.append ( "..\\"+contentValue.replace("/", "\\") )
-        fixPathDest.append ( "../"+os.path.join ( contentDest, contentKey ) )
-        fixPathDest.append ( "..\\"+os.path.join ( contentDest, contentKey ).replace("/", "\\") )
-        print "Moved Content: " + contentValue + " " + contentKey + " " + contentDest
-
-    # on ios this messesup due to the class dir
-    # fixPathOrig.append ( "path = ../classes;" )
-    # fixPathDest.append ( "" )
-
-    # fix up the refrences to the frameworks
+    # call process_proj from each platform's script folder          
     for platform in platforms_list:
-        proj_path = context["dst_project_path"] + "/proj.%s/" % platform
-        replaceStringsInDir (proj_path, fixPathOrig, fixPathDest)
-        replaceStringsInDir (proj_path, [ "../proj.%s/" % platform ], [ "../../proj.%s/" % platform ])
-        print "Fixed up frameworks for platform: " + platform
+        processPlatformProjects(platform)
+    #    exec "import %s.handle_project_files" % (platform)
+    #    exec "%s.handle_project_files.handle_project_files(context)" % (platform)
+
+    # check to see if the path is not the default path
+    if not "/../../projects/" in context["dst_project_path"]:
+        # define the frameworks path
+        frameworksPath = os.path.join ( context["dst_project_path"], frameworksDest )
+        # create a frameworks path
+        os.makedirs ( frameworksPath )
+
+        # relative path to the project
+        fixPathOrig = []
+        fixPathDest = []
+
+        # copy frameworks into the folder
+        for framework in frameworks.keys():
+            frameworkPath = frameworks[framework]
+            sourcePath = context["src_project_path"]+"/"+frameworkPath
+            destinationPath = os.path.join ( frameworksPath, framework )
+            shutil.copytree( sourcePath, destinationPath, symlinks=False, ignore=ignore_patterns('.*'))
+            fixPathOrig.append ( frameworkPath )
+            fixPathOrig.append ( frameworkPath.replace("/", "\\") )
+            fixPathDest.append ( os.path.join ( frameworksDest, framework ) )
+            fixPathDest.append ( os.path.join ( frameworksDest, framework ).replace("/", "\\") )
+            print "Copied framework: " + framework
+
+        #move the content folders
+        contentDest = "proj"
+        contentsPath = os.path.join ( context["dst_project_path"], contentDest )
+        for contentKey in content:
+            contentValue = content[contentKey]
+            sourcePath = context["dst_project_path"]+"/"+contentValue
+            destinationPath = os.path.join ( contentsPath, contentKey )
+            shutil.move( sourcePath, destinationPath )
+            fixPathOrig.append ( "../"+contentValue )
+            fixPathOrig.append ( "..\\"+contentValue.replace("/", "\\") )
+            fixPathDest.append ( "../"+os.path.join ( contentDest, contentKey ) )
+            fixPathDest.append ( "..\\"+os.path.join ( contentDest, contentKey ).replace("/", "\\") )
+            print "Moved Content: " + contentValue + " " + contentKey + " " + contentDest
+
+        # on ios this messesup due to the class dir
+        # fixPathOrig.append ( "path = ../classes;" )
+        # fixPathDest.append ( "" )
+
+        # fix up the refrences to the frameworks
+        for platform in platforms_list:
+            proj_path = context["dst_project_path"] + "/proj.%s/" % platform
+            replaceStringsInDir (proj_path, fixPathOrig, fixPathDest)
+            replaceStringsInDir (proj_path, [ "../proj.%s/" % platform ], [ "../../proj.%s/" % platform ])
+            print "Fixed up frameworks for platform: " + platform
 
 
-print "New project has been created in this path: " + context["dst_project_path"].replace("/tools/project-creator/../..", "")
-print "Have Fun!"
+    print "New project has been created in this path: " + context["dst_project_path"].replace("/tools/project-creator/../..", "")
+    print "Have Fun!"
 
